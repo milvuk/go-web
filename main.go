33d80b8bedda7111cd69f0809d05273657ffde1f
Milan Vukovic <milvuk@gmail.com>
2024-03-13 23:23:01 +0100

    viper env

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+func viperEnvVariable(key string) string {
+	viper.SetConfigFile(".env")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatalf("Error while reading config file %s", err)
+	}
+
+	value, ok := viper.Get(key).(string)
+	if !ok {
+		log.Fatalf("Invalid type assertion")
+	}
+
+	return value
+}
 
 func main() {
-	fmt.Println("tu smo")
+	value := viperEnvVariable("DB_PORT")
+
+	fmt.Println(value)
 }
