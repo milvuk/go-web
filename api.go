903a4fcdfd71613644204f1930444abe64a45e8a
Milan Vukovic <milvuk@gmail.com>
2024-03-17 20:04:24 +0100

    api route POST /albums

--- api.go
+++ api.go
@@ -22,12 +22,14 @@ type APIServer struct {
 func (s *APIServer) run() {
 	http.HandleFunc("GET /albums", s.getAlbumsHandler)
 	http.HandleFunc("GET /albums/{id}", s.getAlbumHandler)
+	http.HandleFunc("POST /albums", s.postAlbumHandler)
 
 	log.Println("API server running at", s.listenAddr)
 	log.Fatal(http.ListenAndServe(s.listenAddr, nil))
 }
 
 func (s *APIServer) getAlbumsHandler(w http.ResponseWriter, r *http.Request) {
+	// todo: pagination, filtering, ordering
 	albums, err := albums(s.db)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -53,4 +55,22 @@ func (s *APIServer) getAlbumHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	writeJson(w, http.StatusOK, album)
+}
+
+func (s *APIServer) postAlbumHandler(w http.ResponseWriter, r *http.Request) {
+	var alb Album
+
+	err := json.NewDecoder(r.Body).Decode(&alb)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+
+	insertId, err := addAlbum(s.db, alb)
+	if err != nil {
+		http.Error(w, "", http.StatusInternalServerError)
+	}
+
+	alb.ID = insertId
+
+	writeJson(w, http.StatusCreated, alb)
 }
